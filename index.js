2db4ea27642059f27593fed57bab68653fac3fc5
Adsimon <adsimon@outlook.fr>
2021-09-30 13:52:05 +0200

    index.js modifié pour appeler la fonctionnalité d'xp
    level.js gère le système d'xp et s'occuper de créer le profil utilisateur dans le fichier json userProfile

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const Discord = require('discord.js');
 //Import token file
 const token = require("./token.json");
 //Import config file
-const config = require("./config.json")
+const config = require("./config.json");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const levelSystem = require("./level.js");
+
 
 //Starting bot
-bot.login(token.token);
-bot.on("ready", async () =>{
+client.login(token.token);
+client.on("ready", async () =>{
     console.log(config.logMessage);
-    bot.user.setStatus(config.status);
-    bot.user.setActivity(config.activity, {type: config.activityType});
+    client.user.setStatus(config.status);
+    client.user.setActivity(config.activity, {type: config.activityType});
+});
+
+client.on('messageCreate', async(message) => {
+
+    if(!message.author.bot){
+        levelSystem.addXp(message);
+    }
+
 });
---
+++ level.js
@@ -0,0 +1,117 @@
+
+//Import discord.js library
+const Discord = require('discord.js');
+//Import config file
+const config = require("./config.json");
+const fs = require('fs');
+const { Console } = require('console');
+const levelIncreaseCoef = 1.5;
+const requiredXp = 1000;
+
+const addXp = async (message) =>{
+
+    //Lecture du fichier des profils utilisateurs
+    fs.readFile('./userProfile.json', (err, data) => {
+
+        if (err) throw err;
+
+        //Conversion en objet Json
+        let profiles = JSON.parse(data);
+
+        //date d'aujourd'hui (dd/mm/yyyy)
+        let currentDate = (new Date()).toLocaleDateString('en-GB');
+       
+        // Variable: id du serveur sur lequel le message a été écrit
+        let guildId = message.guild.id;
+
+        // Variable: id de l'utilisateur qui a écrit le message
+        let authorId = message.author.id;
+       
+        // Si l'utilisateur a un profil enregistré
+        if(profiles.hasOwnProperty(authorId)){
+
+            //Variable: object JSON représentant le profil de l'utilisateur qui a écrit le message
+            let userProfile = profiles[authorId];
+
+            // Si l'utilisateur a un profil enregistré pour ce serveur
+            if(userProfile.hasOwnProperty(guildId)){
+
+                //Variable: object JSON représentant le profil de l'utilisateur sur ce serveur
+                let userGuild = userProfile[guildId];
+
+                //Variable: nombre de point d'xp gagné par l'utilisateur
+                let xpGain = 0;
+
+                //Variable: nombre de points actuel de l'utilisateur
+                let currentXp = userGuild["xp"];
+
+                //Variable: niveau actuel de l'utilisateur
+                let currentLevel = userGuild["level"];
+
+                //Variable: nombre de points d'xp nécessaire pour que l'utilisateur monte de niveau
+                let neededXp = requiredXp + ((currentLevel -1) * levelIncreaseCoef * requiredXp);
+                
+                //Si la date actuelle ne correspond pas à la dernière date de passage de l'utilisateur
+                if(userGuild["lastSeen"] != currentDate){
+
+                    //on met à jour la date de dernier passge sur son profil
+                    profiles[authorId][guildId]["lastSeen"] = currentDate;
+                    console.log("New message of the day !");
+                    //l'xp gagné est de 100
+                    xpGain = 100;
+                }
+                else{
+
+                    //l'xp gagné est de 1
+                    xpGain = 1;
+                }
+
+                //Si l'xp actuelle dépasse l'xp requise pour monter de niveau
+                if(currentXp >= neededXp){
+                    //la valeur level du profil de l'utilisateur pour ce serveur monte de 1
+                    profiles[authorId][guildId]["level"] = currentLevel + 1;
+                }
+
+                //l'xp du profil de l'utilisateur pour ce serveur est mise à jour
+                profiles[authorId][guildId]["xp"] = currentXp + xpGain;
+                console.log("gaining xp!");
+            }
+
+            //L'utilisateur n'a pas de profil pour ce serveur
+            else{
+
+                //Création du profil de serveur
+                profiles[authorId][guildId] = {};
+                profiles[authorId][guildId]["xp"] = 0;
+                profiles[authorId][guildId]["level"] = 1;
+                profiles[authorId][guildId]["lastSeen"] = currentDate;
+                profiles[authorId][guildId]["inventory"] = {};
+
+                console.log("Creating user guild profile");
+            }
+        }
+        //L'utilisateur n'a pas de profil
+        else{
+
+            //Création du profil
+            profiles[authorId] = {};
+            profiles[authorId][guildId] = {};
+            profiles[authorId][guildId]["xp"] = 0;
+            profiles[authorId][guildId]["level"] = 1;
+            profiles[authorId][guildId]["lastSeen"] = currentDate;
+            profiles[authorId][guildId]["inventory"] = {};
+
+            console.log("Creating user profile");
+        }
+
+        //écriture de l'objet JSON modifié dans userProfile.json pour sauvegarder les changements
+        fs.writeFile('./userProfile.json', JSON.stringify(profiles, null, 4), (err) => {
+            if (err) throw err;
+            console.log("done");
+        });        
+    });
+}
+
+
+
+module.exports.addXp = addXp
